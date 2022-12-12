b03b7abb4e81cdfd8dfc2db2060a1832c5648933
diegocolonia <diegocolonia98@yahoo.com>
2022-12-12 13:01:23 -0500

    commit

--- src/main/java/CarolinaEcommerceBackEnd/CarolinaEcommerceBackEnd/model/Product.java
+++ src/main/java/CarolinaEcommerceBackEnd/CarolinaEcommerceBackEnd/model/Product.java
@@ -1,8 +1,11 @@
 package CarolinaEcommerceBackEnd.CarolinaEcommerceBackEnd.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import java.util.Date;
 import java.util.List;
 import java.util.Set;
 
@@ -20,29 +23,50 @@ public class Product {
     private  String productName;
     @Column(name="description")
     private String description;
-
     @Column(name="imageUrl")
     private String imageUrl;
+
+    @Column(name="productPrice")
+    private int productPrice;
     @ManyToOne
     @JoinColumn(name="category_id")
     private ProductCategory productCategory;
+
+    @Column(name = "date_created")
+    @CreationTimestamp
+    private Date dateCreated;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @Column(name = "last_updated")
+    @UpdateTimestamp
+    private Date lastUpdated;
+
+    @Column(name = "units_in_stock")
+    private int unitsInStock;
 
     @ManyToOne
     @JoinColumn(name="attachment_id")
     private Attachment attachmentFile;
 
 
-    public Product(int productId, String productName, String description, ProductCategory productCategory, Attachment attachmentFile) {
+    public Product(int productId, String productName, String description, String imageUrl, int productPrice, ProductCategory productCategory, Date dateCreated, boolean active, Date lastUpdated, int unitsInStock, Attachment attachmentFile) {
         this.productId = productId;
         this.productName = productName;
         this.description = description;
+        this.imageUrl = imageUrl;
+        this.productPrice = productPrice;
         this.productCategory = productCategory;
+        this.dateCreated = dateCreated;
+        this.active = active;
+        this.lastUpdated = lastUpdated;
+        this.unitsInStock = unitsInStock;
         this.attachmentFile = attachmentFile;
     }
 
     public Product() {
     }
-
 
 
     @JsonBackReference
@@ -55,6 +79,14 @@ public class Product {
         return attachmentFile;
     }
 
+    public int getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(int productPrice) {
+        this.productPrice = productPrice;
+    }
+
     public void setAttachmentFile(Attachment attachmentFile) {
         this.attachmentFile = attachmentFile;
     }
@@ -62,6 +94,7 @@ public class Product {
     public String getImageUrl() {
         return imageUrl;
     }
+
 
     public void setImageUrl(String imageUrl) {
         this.imageUrl = imageUrl;
@@ -95,5 +128,35 @@ public class Product {
         this.productCategory = productCategory;
     }
 
+    public Date getDateCreated() {
+        return dateCreated;
+    }
 
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    public int getUnitsInStock() {
+        return unitsInStock;
+    }
+
+    public void setUnitsInStock(int unitsInStock) {
+        this.unitsInStock = unitsInStock;
+    }
 }
